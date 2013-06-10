a7a42601e19d050bfce226400cef1dd614f9f93b
Unia <jthidskes@live.nl>
2013-06-10 19:26:14 +0200

    removed gtk2 support, imported into anjuta

--- src/gtimer.c
+++ src/gtimer.c
@@ -1,24 +1,26 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
- * gtimer
- * Copyright (C) 2013 Jente (jthidskes at outlook dot com)
- *
- * gtimer is free software: you can redistribute it and/or modify it
+ * gtimer.c
+ * Copyright (C) 2013 Jente Hidskes <jthidskes@outlook.com>
+ * 
+ * gtimeutils is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * gtimer is distributed in the hope that it will be useful, but
+ * 
+ * gtimeutils is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <config.h>
 #include <gtk/gtk.h>
 #include <canberra.h>
+#include <glib/gi18n.h>
 #include <glib/gprintf.h>
 #include <libnotify/notify.h>
 #include <stdlib.h>
@@ -165,11 +167,12 @@ int main (int argc, char *argv[]) {
 	GError *error_parsearg = NULL;
 	GOptionContext *context;
 	GtkWidget *window, *vbox, *hbox2;
-#ifdef GTK3
 	GtkAdjustment *sadj, *madj, *hadj;
-#endif
-#ifdef GTK2
-	GtkObject *sadj, *madj, *hadj;
+
+#ifdef ENABLE_NLS
+	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
 #endif
 
 	gtk_init(&argc, &argv);
@@ -183,16 +186,9 @@ int main (int argc, char *argv[]) {
 		exit(1);
 	}
 
-#ifdef GTK3
 	vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
 	hbox1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 	hbox2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-#endif
-#ifdef GTK2
-	vbox = gtk_vbox_new(FALSE, 5);
-	hbox1 = gtk_hbox_new(FALSE, 0);
-	hbox2 = gtk_hbox_new(FALSE, 0);
-#endif
 
 	gtk_box_set_spacing(GTK_BOX(hbox2), 5);
 	gtk_container_set_border_width(GTK_CONTAINER(vbox), 5);
@@ -203,16 +199,9 @@ int main (int argc, char *argv[]) {
 	sadj = gtk_adjustment_new(0, 0, 60, 1, 1, 0);
 	madj = gtk_adjustment_new(0, 0, 60, 1, 1, 0);
 	hadj = gtk_adjustment_new(0, 0, 24, 1, 1, 0);
-#ifdef GTK3
 	spin_seconds = gtk_spin_button_new(sadj, 1, 0);
 	spin_minutes = gtk_spin_button_new(madj, 1, 0);
 	spin_hours = gtk_spin_button_new(hadj, 1, 0);
-#endif
-#ifdef GTK2
-	spin_seconds = gtk_spin_button_new(GTK_ADJUSTMENT(sadj), 1, 0);
-	spin_minutes = gtk_spin_button_new(GTK_ADJUSTMENT(madj), 1, 0);
-	spin_hours = gtk_spin_button_new(GTK_ADJUSTMENT(hadj), 1, 0);
-#endif
 
 	gtk_spin_button_set_wrap(GTK_SPIN_BUTTON(spin_seconds), TRUE);
 	gtk_spin_button_set_value(GTK_SPIN_BUTTON(spin_seconds), seconds);
@@ -256,5 +245,6 @@ int main (int argc, char *argv[]) {
 	gtk_main();
 	notify_uninit();
 	ca_context_destroy(sound);
-	g_option_context_free(context); 
+	g_option_context_free(context);
+	return 0;
 }
